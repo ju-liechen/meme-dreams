a138bf6a1673610877c884090a6160ed68bc78a2
ju-liechen <juliechen4649@gmail.com>
2024-11-04 16:21:31 -0400

    Add BaseLayout in progress

---
+++ components/navigation/index.js
@@ -0,0 +1,10 @@
+import styles from './navigation.module.scss'
+
+export const Navigation = () => {
+  return (
+  <div className={styles['nav-wrapper']}>
+    <h1>Meme Dreams</h1>
+    <span>About</span>
+    </div>
+  )
+}
