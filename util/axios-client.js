34f15a6b0f79d52222b9a53f3ec13f9834055140
ju-liechen <juliechen4649@gmail.com>
2024-11-05 11:44:45 -0400

    Update axiosClient

--- util/axios-client.js
+++ util/axios-client.js
@@ -1,6 +1,25 @@
 import axios from 'axios'
 
-export const axiosClient = axios.create({
-  baseURL: '/api',
-  headers: {'Content-Type': 'application/json'},
+const axiosClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    'x-rapidapi-host': 'ronreiter-meme-generator.p.rapidapi.com',
+    'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPIDAPI_KEY,
+  },
 })
+
+axiosClient.interceptors.response.use(
+  (response) => {
+    // This function simply returns the response as is, without modification.
+    // It's a placeholder for potentially transforming response data in the future.
+    return response
+  },
+  (error) => {
+    // This function catches any errors from the response.
+    // Similar to the request interceptor, it forwards the error to be handled by .catch().
+    return Promise.reject(error)
+  }
+)
+
+export { axiosClient }
