a45b189283781d19850b13741fef59ea49b57b9e
ju-liechen <juliechen4649@gmail.com>
2024-11-06 16:51:32 -0400

    Add modal to BaseLayout and home page

--- components/layouts/base-layout.js
+++ components/layouts/base-layout.js
@@ -1,4 +1,6 @@
 import ErrorBoundary from 'components/error-boundary'
+import { Modals } from 'components/modal'
+import { UploadImage } from 'components/modal/upload-image'
 import { Navigation } from 'components/navigation'
 import { Notification } from 'components/notification/'
 import { SvgDefs } from 'components/svg-defs'
@@ -8,6 +10,7 @@ import styles from './base-layout.module.scss'
 export const BaseLayout = ({ children }) => {
   return (
     <>
+      <Modals modals={{ UploadImage }}/>
       <div className={styles['base-container']}>
         <Notification />
         <Navigation />
